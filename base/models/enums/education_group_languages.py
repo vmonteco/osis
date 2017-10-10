ff9c229f2c95dbaa9905cf2fd6493a6f6e4a5ec1
defat <defat.david@gmail.com>
2017-10-10 16:07:24 +0200

    OSIS-13 Replace in education_group_year.py the attribute 'primary_language' by an enum file 'education_group_languagues

---
+++ base/models/enums/education_group_languages.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2017 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+EN = "ENGLISH"
+FR = "FRENCH"
+FR_OR_EN = "FRENCH_OR_ENGLISH"
+FR_AND_EN = "FRENCH_AND_ENGLISH"
+
+LANGUAGES = (
+    (EN, EN),
+    (FR, FR),
+    (FR_OR_EN, FR_OR_EN),
+    (FR_AND_EN, FR_AND_EN),
+)
