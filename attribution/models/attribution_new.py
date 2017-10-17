3eaeb9658ae3522ae342ef5bc5d6feec3dc87ee5
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2017-10-17 17:52:36 +0200

    #OSIS-208 Attribution model adaptation

--- attribution/models/attribution_new.py
+++ attribution/models/attribution_new.py
@@ -27,14 +27,16 @@ from django.db import models
 
 from osis_common.models.auditable_model import AuditableModelAdmin, AuditableModel
 
+from attribution.models.enums import function
+
 
 class AttributionNewAdmin(AuditableModelAdmin):
-    list_display = ('tutor', 'score_responsible', 'learning_container_year', 'start_year', 'end_year', 'changed')
+    list_display = ('tutor', 'score_responsible', 'function', 'learning_container_year', 'start_year', 'end_year', 'changed')
     list_filter = ('learning_container_year__academic_year', 'score_responsible')
     fieldsets = ((None, {'fields': ('learning_container_year', 'tutor', 'score_responsible', 'start_year', 'end_year')}),)
     raw_id_fields = ('learning_container_year', 'tutor')
     search_fields = ['tutor__person__first_name', 'tutor__person__last_name', 'learning_container_year__acronym',
-                     'tutor__person__global_id']
+                     'tutor__person__global_id', 'function']
 
 
 class AttributionNew(AuditableModel):
@@ -42,13 +44,12 @@ class AttributionNew(AuditableModel):
     changed = models.DateTimeField(null=True, auto_now=True)
     learning_container_year = models.ForeignKey('base.LearningContainerYear')
     tutor = models.ForeignKey('base.Tutor')
+    function = models.CharField(max_length=35, blank=True, null=True, choices=function.FUNCTIONS, db_index=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
     start_year = models.IntegerField(blank=True, null=True)
     end_year = models.IntegerField(blank=True, null=True)
     score_responsible = models.BooleanField(default=False)
-    volume_cm = models.DecimalField(max_digits=6, decimal_places=1, blank=True, null=True)
-    volume_tp = models.DecimalField(max_digits=6, decimal_places=1, blank=True, null=True)
 
     def __str__(self):
-        return u"%s" % self.tutor.person
+        return u"%s - %s" % (self.tutor.person, self.function)
