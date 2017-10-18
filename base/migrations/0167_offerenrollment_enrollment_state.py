bd2e79d6743c667903a41d3f6eceb62b0d8e6f09
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2017-10-18 10:06:58 +0200

    fixed migration file order

--- base/migrations/0167_offerenrollment_enrollment_state.py
+++ base/migrations/0167_offerenrollment_enrollment_state.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0164_educationgroupyear_category'),
+        ('base', '0166_educationgrouporganization'),
     ]
 
     operations = [
