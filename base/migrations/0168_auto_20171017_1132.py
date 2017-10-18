bc9ad9238b55a7ebae1797ddc88d281ab72873c0
Christophe Fivet <christophe.fivet@uclouvain.be>
2017-10-18 13:04:14 +0200

    #OSIS-39 Correction migration files after merge

--- base/migrations/0168_auto_20171017_1132.py
+++ base/migrations/0168_auto_20171017_1132.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0166_educationgrouporganization'),
+        ('base', '0167_offerenrollment_enrollment_state'),
     ]
 
     operations = [
