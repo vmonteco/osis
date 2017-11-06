b2d857fdec4f6f14828003b890fd270f3835d4bb
Aurelien De Ryck <aurelien.deryck@usaintlouis.be>
2017-11-06 16:08:37 +0100

    Unit test for tutor model

--- base/tests/models/test_tutor.py
+++ base/tests/models/test_tutor.py
@@ -23,7 +23,11 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from base.tests.models import test_person
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.models import Group
+from django.contrib.messages.api import get_messages
+from django.contrib.messages.storage.fallback import FallbackStorage
+
 from base.models import tutor
 from django.test import TestCase
 
@@ -31,21 +35,24 @@ from base.tests.factories.tutor import TutorFactory
 from base.tests.factories.person import PersonFactory
 from base.tests.factories.user import UserFactory
 
+
 def create_tutor(first_name="Tutor", last_name="tutor"):
     return TutorFactory(
-        person = PersonFactory(first_name=first_name, last_name=last_name)
+        person=PersonFactory(first_name=first_name, last_name=last_name)
     )
+
 
 def create_tutor_with_person(person):
     return TutorFactory(person=person)
 
-class TestTutor(TestCase) :
+
+class TestTutor(TestCase):
     def setUp(self):
         self.user = UserFactory()
         self.person = PersonFactory(first_name="James", last_name="Dupont", user=self.user)
         self.tutor = TutorFactory(person=self.person)
-        TutorFactory() #Create fake Tutor
-        TutorFactory() #Create fake Tutor
+        TutorFactory()  # Create fake Tutor
+        TutorFactory()  # Create fake Tutor
 
     def test_find_by_person(self):
         self.assertEqual(self.tutor, tutor.find_by_person(self.person))
@@ -63,3 +70,61 @@ class TestTutor(TestCase) :
     def test_is_not_tutor(self):
         user_unknown = UserFactory()
         self.assertFalse(tutor.is_tutor(user_unknown))
+
+    def test_find_by_user(self):
+        self.assertEqual(self.tutor, tutor.find_by_user(self.user))
+
+    def test_find_by_user_wrong_id(self):
+        self.assertIsNone(tutor.find_by_user(-1))
+
+    def test_find_by_id(self):
+        self.assertEqual(self.tutor, tutor.find_by_id(self.tutor.id))
+
+    def test_find_by_id_wrong_id(self):
+        self.assertIsNone(tutor.find_by_id(-1))
+
+
+class MockRequest:
+    COOKIES={}
+
+
+class MockSuperUser:
+    def has_perm(self, perm):
+        return True
+
+
+request = MockRequest()
+request.user = MockSuperUser()
+
+
+class TestTutorAdmin(TestCase):
+    def setUp(self):
+        for _ in range(10):
+            user = UserFactory()
+            person = PersonFactory(user=user)
+            TutorFactory(person=person)
+            user.groups.clear()
+
+        self.site = AdminSite()
+
+    def test_add_to_group(self):
+        setattr(request, 'session', 'session')
+        msg = FallbackStorage(request)
+        setattr(request, '_messages', msg)
+        ma = tutor.TutorAdmin(tutor.Tutor, self.site)
+        queryset = tutor.Tutor.objects.all()
+        ma.add_to_group(request, queryset)
+        msg = [m.message for m in get_messages(request)]
+        self.assertIn("{} users added to the group 'tutors'.".format(10), msg)
+
+    def test_add_to_group_no_tutor_group(self):
+        setattr(request, 'session', 'session')
+        msg = FallbackStorage(request)
+        setattr(request, '_messages', msg)
+        for group in Group.objects.all():
+            group.delete()
+        ma = tutor.TutorAdmin(tutor.Tutor, self.site)
+        queryset = tutor.Tutor.objects.all()
+        ma.add_to_group(request, queryset)
+        msg = [m.message for m in get_messages(request)]
+        self.assertIn("Group tutors doesn't exist.", msg)
