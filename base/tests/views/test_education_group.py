5a6aaf2b64e42534fee4d5fc6c306b8ec34581a4
Christophe Fivet <christophe.fivet@uclouvain.be>
2017-11-21 09:13:20 +0100

    #OSIS-238 Correction des tests

--- base/tests/views/test_education_group.py
+++ base/tests/views/test_education_group.py
@@ -154,7 +154,9 @@ class EducationGroupViewTestCase(TestCase):
     @mock.patch('django.contrib.auth.decorators')
     @mock.patch('base.views.layout.render')
     @mock.patch('base.models.person.get_user_interface_language', return_value=True)
+    @mock.patch('base.models.education_group_year.find_by_id')
     def test_education_group_general_informations(self,
+                                                  mock_find_by_id,
                                                   mock_get_user_interface_language,
                                                   mock_render,
                                                   mock_decorators):
@@ -165,6 +167,7 @@ class EducationGroupViewTestCase(TestCase):
 
         from base.views.education_group import education_group_general_informations
 
+        mock_find_by_id.return_value = education_group_year
         education_group_general_informations(request, education_group_year.id)
         self.assertTrue(mock_render.called)
         request, template, context = mock_render.call_args[0]
@@ -172,7 +175,9 @@ class EducationGroupViewTestCase(TestCase):
 
     @mock.patch('django.contrib.auth.decorators')
     @mock.patch('base.views.layout.render')
+    @mock.patch('base.models.education_group_year.find_by_id')
     def test_education_administrative_data(self,
+                                           mock_find_by_id,
                                            mock_render,
                                            mock_decorators):
         mock_decorators.login_required = lambda x: x
@@ -182,6 +187,7 @@ class EducationGroupViewTestCase(TestCase):
 
         from base.views.education_group import education_group_administrative_data
 
+        mock_find_by_id.return_value = education_group_year
         education_group_administrative_data(request, education_group_year.id)
         self.assertTrue(mock_render.called)
         request, template, context = mock_render.call_args[0]
