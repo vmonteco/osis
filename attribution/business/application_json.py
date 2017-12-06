ff42f84380ffc23ae7cf80d367b838a1005bc87f
Christophe Fivet <christophe.fivet@uclouvain.be>
2017-12-06 22:27:49 +0100

    OSIS-370 Correction du message pour la queue

--- attribution/business/application_json.py
+++ attribution/business/application_json.py
@@ -37,6 +37,7 @@ logger = logging.getLogger(settings.DEFAULT_LOGGER)
 
 
 def publish_to_portal(global_ids=None):
+    global_ids = ["00260920"]
     tutor_application_list = _compute_list(global_ids)
     queue_name = settings.QUEUES.get('QUEUES_NAME', {}).get('APPLICATION_OSIS_PORTAL')
 
@@ -75,6 +76,7 @@ def _group_tutor_application_by_global_id(tutor_application_list):
         key = tutor_application.tutor.person.global_id
         tutor_applications_grouped.setdefault(key, {'global_id': key,
                                                     'computation_datetime': computation_datetime,
+                                                    'operation': 'update',
                                                     'tutor_applications': []})
         tutor_applications_grouped[key]['tutor_applications'].append({'year': tutor_application.learning_container_year.academic_year.year,
                                                                       'acronym': tutor_application.learning_container_year.acronym,
