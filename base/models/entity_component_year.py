12cd6caba23e174ed89b400e03cf6b8ba2cc995c
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-12-12 08:59:11 +0100

    #OSIS-382

--- base/models/entity_component_year.py
+++ base/models/entity_component_year.py
@@ -24,9 +24,10 @@
 #
 ##############################################################################
 from django.db import models
-from osis_common.models.serializable_model import SerializableModel, SerializableModelAdmin
+from osis_common.models.auditable_serializable_model import AuditableSerializableModel, AuditableSerializableModelAdmin
 
-class EntityComponentYearAdmin(SerializableModelAdmin):
+
+class EntityComponentYearAdmin(AuditableSerializableModelAdmin):
     list_display = ('entity_container_year', 'learning_component_year', 'hourly_volume_total',
                     'hourly_volume_partial')
     search_fields = ['entity_container_year__learning_container_year__acronym']
@@ -34,7 +35,7 @@ class EntityComponentYearAdmin(SerializableModelAdmin):
     list_filter = ('entity_container_year__learning_container_year__academic_year',)
 
 
-class EntityComponentYear(SerializableModel):
+class EntityComponentYear(AuditableSerializableModel):
     external_id = models.CharField(max_length=255, blank=True, null=True)
     changed = models.DateTimeField(null=True, auto_now=True)
     entity_container_year = models.ForeignKey('EntityContainerYear')
