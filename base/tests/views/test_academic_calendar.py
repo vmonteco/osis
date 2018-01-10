cb29048ea2be5c2cf6aee1d310fb78dad6bb52c6
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2018-01-10 13:22:35 +0100

    fixed unit test failure

--- base/tests/views/test_academic_calendar.py
+++ base/tests/views/test_academic_calendar.py
@@ -166,5 +166,5 @@ class AcademicCalendarViewTestCase(TestCase):
 
         self.assertTrue(mock_render.called)
         request, template, context = mock_render.call_args[0]
-        self.assertEqual(template, 'academic_calendar_form.html')
+        self.assertEqual(template, 'academic_calendar.html')
 
