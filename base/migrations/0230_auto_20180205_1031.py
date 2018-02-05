25026229f50b9cc6bac0a515cacc4427f30d901b
Ndizera Eddy <eddyndizera@gmail.com>
2018-02-05 15:48:16 +0100

    OSIS-636 Modified migration dependence of base/230

--- base/migrations/0230_auto_20180205_1031.py
+++ base/migrations/0230_auto_20180205_1031.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0227_auto_20180202_1545'),
+        ('base', '0229_auto_20180205_1048'),
     ]
 
     operations = [
