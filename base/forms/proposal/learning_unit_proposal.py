b5fa9089e62eebda7ccdf6412e787143eff83979
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-03-01 15:22:26 +0100

    OSIS-747 Correction is_valid super u

--- base/forms/proposal/learning_unit_proposal.py
+++ base/forms/proposal/learning_unit_proposal.py
@@ -77,9 +77,6 @@ class LearningUnitProposalForm(SearchForm):
     )
 
     def is_valid(self):
-        if not super().is_valid():
-            return False
-
         if not self._has_criteria():
             self.add_error(None, _('minimum_one_criteria'))
             return False
