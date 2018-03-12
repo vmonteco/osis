78ae5b402bfb2db1475d41563da11fde1a5a5cb8
Laurent Buset <laurent.buset@uclouvain.be>
2018-03-12 15:59:26 +0100

    use entity instead of entity_version in ReviewerDelegationForm

--- assistant/views/reviewer_delegation.py
+++ assistant/views/reviewer_delegation.py
@@ -30,6 +30,7 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
+from django.views.decorators.http import require_http_methods
 from django.views.generic import ListView
 
 from base.models import academic_year, person, entity, entity_version
@@ -61,10 +62,10 @@ class StructuresListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
                                                                        academic_year.current_academic_year())) > 0:
             self.is_supervisor = True
         rev = reviewer.find_by_person(self.request.user.person)
-        main_entity_version = entity_version.get_last_version(rev.entity)
-        queryset = list(main_entity_version.find_direct_children())
-        queryset.insert(0, main_entity_version)
-        return queryset
+        entities_version = entity_version.get_last_version(rev.entity).children
+        entities = [this_entity_version.entity.id for this_entity_version in entities_version]
+        entities.insert(0, entity_version.get_last_version(rev.entity).entity.id)
+        return entity.find_versions_from_entites(entities, date=None)
 
     def get_context_data(self, **kwargs):
         context = super(StructuresListView, self).get_context_data(**kwargs)
@@ -85,55 +86,44 @@ def user_is_reviewer_and_can_delegate(user):
         return False
 
 
+@require_http_methods(["POST"])
 @user_passes_test(user_is_reviewer_and_can_delegate, login_url='assistants_home')
 def add_reviewer_for_structure(request):
-    entity_id = request.POST.get("entity_id")
-    related_entity = entity.get_by_internal_id(entity_id)
-    current_entity_version = entity_version.get_last_version(entity.get_by_internal_id(entity_id))
+    current_entity = entity.find_by_id(request.POST.get("entity"))
     year = academic_year.current_academic_year().year
-    try:
-        reviewer.can_delegate_to_entity(reviewer.find_by_person(request.user.person), related_entity)
-    except:
+    if not reviewer.can_delegate_to_entity(reviewer.find_by_person(request.user.person), current_entity):
         return redirect('assistants_home')
-    if request.POST:
-        form = ReviewerDelegationForm(data=request.POST)
-        if form.is_valid():
-            new_reviewer = form.save(commit=False)
-            if request.POST.get('person_id'):
-                this_person = person.find_by_id(request.POST.get('person_id'))
-                try:
-                    reviewer.find_by_person(this_person)
-                    msg = _("person_already_reviewer_msg")
-                    form.add_error(None, msg)
-                    return render(request, "reviewer_add_reviewer.html", {
-                        'form': form,
-                        'year': year,
-                        'related_entity': related_entity,
-                        'current_entity_version': current_entity_version,
-                        'reviewer': reviewer.find_by_person(request.user.person)
-                    })
-                except reviewer.Reviewer.DoesNotExist:
-                    pass
-                new_reviewer.person = this_person
-                new_reviewer.save()
-                html_template_ref = 'assistant_reviewers_startup_html'
-                txt_template_ref = 'assistant_reviewers_startup_txt'
-                send_message(person=this_person, html_template_ref=html_template_ref,
-                             txt_template_ref=txt_template_ref)
-                return redirect('reviewer_delegation')
+    form = ReviewerDelegationForm(data=request.POST)
+    if form.is_valid():
+        new_reviewer = form.save(commit=False)
+        if request.POST.get('person_id'):
+            this_person = person.find_by_id(request.POST.get('person_id'))
+            if reviewer.find_by_person(this_person):
+                msg = _("person_already_reviewer_msg")
+                form.add_error(None, msg)
+                return render(request, "reviewer_add_reviewer.html", {
+                    'form': form,
+                    'year': year,
+                    'entity': current_entity,
+                    'reviewer': reviewer.find_by_person(request.user.person)
+                })
+            new_reviewer.person = this_person
+            new_reviewer.save()
+            html_template_ref = 'assistant_reviewers_startup_html'
+            txt_template_ref = 'assistant_reviewers_startup_txt'
+            send_message(person=this_person, html_template_ref=html_template_ref,
+                         txt_template_ref=txt_template_ref)
+            return redirect('reviewer_delegation')
+    else:
+        this_reviewer = reviewer.find_by_person(person=request.user.person)
+        if this_reviewer.role == reviewer_role.SUPERVISION:
+            role = reviewer_role.SUPERVISION_ASSISTANT
         else:
-            this_reviewer = reviewer.find_by_person(person=request.user.person)
-            if this_reviewer.role == reviewer_role.SUPERVISION:
-                role = reviewer_role.SUPERVISION_ASSISTANT
-            else:
-                role = reviewer_role.RESEARCH_ASSISTANT
-            form = ReviewerDelegationForm(initial={'entity': related_entity, 'year': year, 'role': role})
-            reviewer_entity = entity_version.get_last_version(this_reviewer.entity)
-            return render(request, "reviewer_add_reviewer.html", {
-                'form': form,
-                'year': year,
-                'related_entity': related_entity,
-                'entity': reviewer_entity,
-                'current_entity_version': current_entity_version,
-                'reviewer': reviewer.find_by_person(request.user.person)
-            })
+            role = reviewer_role.RESEARCH_ASSISTANT
+        form = ReviewerDelegationForm(initial={'entity': current_entity, 'year': year, 'role': role})
+        return render(request, "reviewer_add_reviewer.html", {
+            'form': form,
+            'year': year,
+            'entity': current_entity,
+            'reviewer': reviewer.find_by_person(request.user.person)
+        })
