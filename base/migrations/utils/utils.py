50a9aa3af4765729942c729bc3ecaf6b5807f5dd
Aurelien De Ryck <deryck.aurelien@gmail.com>
2018-03-16 09:55:03 +0100

    OSIS-844 Add fixture permission in migration file

--- base/migrations/utils/utils.py
+++ base/migrations/utils/utils.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.core.exceptions import FieldDoesNotExist
 
 import uuid
+
+from django.core.exceptions import FieldDoesNotExist
+from django.core.management import call_command
+
+JSON_FILE = 'permission'
+
 
 def set_uuids_model(apps, model):
     base = apps.get_app_config('base')
@@ -14,3 +19,7 @@ def set_uuids_model(apps, model):
                 model_class.objects.filter(pk=pk).update(uuid=uuid.uuid4())
             except FieldDoesNotExist:
                 break
+
+
+def initialize_perms(*_):
+    call_command('loaddata', "{}.json".format(JSON_FILE))
