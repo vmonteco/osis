4f0967b92b4e998f3eacbf22258d8516ebdba4a6
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-03-28 15:26:04 +0200

    Correction PEP8

--- base/business/learning_units/perms.py
+++ base/business/learning_units/perms.py
@@ -77,18 +77,19 @@ def _is_attached_to_initial_entity(learning_unit_proposal, a_person):
 def is_eligible_to_edit_proposal(proposal, person):
     if not proposal:
         return False
-
-    if person.is_faculty_manager():
-        if not person.is_linked_to_entity_in_charge_of_learning_unit_year(proposal.learning_unit_year):
-            return False
-
-        elif proposal.state != ProposalState.FACULTY.name:
-            return False
-        elif proposal.type == ProposalType.MODIFICATION.name and\
-                proposal.learning_unit_year.academic_year.year != current_academic_year().year+1:
-            return False
-
+    if person.is_faculty_manager() and not _check_eligible_to_edit_proposal_as_faculty_manager(proposal, person):
+        return False
     return person.user.has_perm('base.can_edit_learning_unit_proposal')
+
+def _check_eligible_to_edit_proposal_as_faculty_manager(proposal, person):
+    if not person.is_linked_to_entity_in_charge_of_learning_unit_year(proposal.learning_unit_year):
+        return False
+    if proposal.state != ProposalState.FACULTY.name:
+        return False
+    if (proposal.type == ProposalType.MODIFICATION.name
+            and proposal.learning_unit_year.academic_year.year != current_academic_year().year + 1):
+        return False
+    return True
 
 
 def is_eligible_for_modification_end_date(learning_unit_year, person):
