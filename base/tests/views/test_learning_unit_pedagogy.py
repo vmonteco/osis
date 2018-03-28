a9f965583e757babb1151a42e874c8a4d775b806
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-03-28 15:58:22 +0200

    Correction test

--- base/tests/views/test_learning_unit_pedagogy.py
+++ base/tests/views/test_learning_unit_pedagogy.py
@@ -94,7 +94,7 @@ class LearningUnitViewPedagogyTestCase(TestCase):
         request, template, context = mock_render.call_args[0]
         self.assertEqual(template, 'learning_units.html')
         self.assertEqual(context['search_type'], SUMMARY_LIST)
-        #FIXME
+        #FIXME Return 1 but 0 normally
         self.assertEqual(len(context['learning_units']), 1)
 
     @mock.patch('base.views.layout.render')
