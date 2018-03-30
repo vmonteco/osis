aa2d42a3879d48fb98f66b34e2a8517cf3a01ec8
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-03-30 15:56:01 +0200

    OSIS-911 Correctrion d'un bug qui ne mettait pas le type suppression

--- base/forms/learning_unit_proposal.py
+++ base/forms/learning_unit_proposal.py
@@ -37,6 +37,7 @@ from base.models.entity_version import find_main_entities_version, get_last_vers
 from base.models.enums import learning_container_year_types, entity_container_year_link_type
 from base.models.enums import proposal_state, proposal_type
 from base.models.enums.entity_container_year_link_type import ENTITY_TYPE_LIST
+from base.models.enums.proposal_type import ProposalType
 from base.models.proposal_learning_unit import ProposalLearningUnit
 
 
@@ -61,13 +62,16 @@ class ProposalLearningUnitForm(forms.ModelForm):
         else:
             self.fields['state'].disabled = True
             self.fields['state'].required = False
+        self.fields['type'].disabled = True
+        self.fields['type'].required = False
+        self.fields['type'].initial = ProposalType.SUPPRESSION.name
 
     def clean_entity(self):
         return self.cleaned_data['entity'].entity
 
     class Meta:
         model = ProposalLearningUnit
-        fields = ['entity', 'folder_id', 'state']
+        fields = ['entity', 'folder_id', 'state', 'type']
 
     def save(self, commit=True):
         if self.instance.initial_data:
