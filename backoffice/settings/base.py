532ccca49223f0c725f031c9be4ee96ce42bccc6
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2018-04-16 15:39:24 +0200

    Fix ATOMIC_REQUESTS property

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -125,7 +125,7 @@ DATABASES = {
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'osis'),
         'HOST': os.environ.get("POSTGRES_HOST", '127.0.0.1'),
         'PORT': os.environ.get("POSTGRES_PORT", '5432'),
-        'ATOMIC_REQUEST':  os.environ.get('DATABASE_ATOMIC_REQUEST', 'False').lower() == 'true'
+        'ATOMIC_REQUESTS':  os.environ.get('DATABASE_ATOMIC_REQUEST', 'False').lower() == 'true'
     },
 }
 
