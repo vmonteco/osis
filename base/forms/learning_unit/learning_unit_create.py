d8dca54341aed2a4e676556d7731d33f668b858b
Aurelien De Ryck <deryck.aurelien@gmail.com>
2018-04-23 14:44:20 +0200

    OSIS-1042 use subtype to split acronym

--- base/forms/learning_unit/learning_unit_create.py
+++ base/forms/learning_unit/learning_unit_create.py
@@ -107,7 +107,7 @@ class LearningUnitYearModelForm(forms.ModelForm):
 
         acronym = self.initial.get('acronym')
         if acronym:
-            self.initial['acronym'] = split_acronym(acronym)
+            self.initial['acronym'] = split_acronym(acronym, subtype)
 
         if subtype == learning_unit_year_subtypes.PARTIM:
             self.fields['acronym'] = PartimAcronymField()
