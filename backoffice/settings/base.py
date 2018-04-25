66785120837c7419d75509c9b812a088e1ef8e82
Ndizera Eddy <eddyndizera@gmail.com>
2018-04-25 11:20:32 +0200

    redis configuration
    	modified:   .env.example
    	modified:   backoffice/settings/base.py
    	modified:   requirements.txt

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -309,3 +309,20 @@ BOOTSTRAP3 = {
     'set_placeholder': False,
     'success_css_class': ''
 }
+
+CACHE_ENABLED = os.environ.get("CACHE_ENABLED", 'False').lower() == 'true'
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "redis://" + os.environ.get("REDIS_HOST", '127.0.0.1') + ":"
+                                   + os.environ.get("REDIS_PORT", '6379'),
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
+                "SOCKET_CONNECT_TIMEOUT": 2,
+                "SOCKET_TIMEOUT": 2,
+            },
+            "KEY_PREFIX": os.environ.get("REDIS_PREFIX", 'osis')
+        }
+}
