bfe79b87e64d2fbb33532cee5c637587fd2a1022
Aurelien De Ryck <deryck.aurelien@gmail.com>
2018-05-03 09:44:26 +0200

    OSIS-1034 Fix unit test

--- base/views/learning_units/update.py
+++ base/views/learning_units/update.py
@@ -141,10 +141,9 @@ def _save_form_and_display_messages(request, form):
     records = None
     try:
         records = form.save()
+        display_warning_messages(request, getattr(form, 'warnings', []))
         display_success_messages(request, _('success_modification_learning_unit'))
-        warnings = form.warnings
-        if warnings:
-            display_warning_messages(request, warnings)
+
     except ConsistencyError as e:
         error_list = e.error_list
         error_list.insert(0, _('The learning unit has been updated until %(year)s.')
