3973757774bd6a6307f86b446a69865cf0efd100
Stephane Wirtel <stephane@wirtel.be>
2018-05-04 13:07:36 +0200

    Refactor the tests PEP8

--- webservices/tests.py
+++ webservices/tests.py
@@ -51,9 +51,31 @@ class ReddotWebServiceTestCase(TestCase):
         })
 
     def _test_education_group_year_with_translation(self, language, iso_language):
+        values = self._create_records_for_test_education_group_year(iso_language)
+
+        academic_year, education_group_year, text_label, translated_text, translated_text_label = values
+
+        response = self._get_response(academic_year.year, language, education_group_year.acronym)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+        response_json = json.loads(response.content.decode('utf-8'))
+
+        context = {
+            'academic_year': academic_year,
+            'education_group_year': education_group_year,
+            'language': language,
+            'text_label': text_label,
+            'translated_text': translated_text,
+            'translated_text_label': translated_text_label
+        }
+
+        fake_response = self._mock_response_dictionary(**context)
+        self.assertDictEqual(response_json, fake_response)
+
+    def _create_records_for_test_education_group_year(self, iso_language):
         academic_year = AcademicYearFactory()
         education_group_year = EducationGroupYearFactory(academic_year=academic_year)
-
         text_label = TextLabelFactory(entity=OFFER_YEAR)
         translated_text_label = TranslatedTextLabelFactory(language=iso_language,
                                                            text_label=text_label)
@@ -64,29 +86,19 @@ class ReddotWebServiceTestCase(TestCase):
             language=iso_language,
             # text is randomized
         )
+        return academic_year, education_group_year, text_label, translated_text, translated_text_label
 
-        response = self._get_response(academic_year.year, language, education_group_year.acronym)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, 'application/json')
-        response_json = json.loads(response.content.decode('utf-8'))
-
-        fake_response = self._mock_response_dictionary(academic_year, education_group_year, language, text_label,
-                                                       translated_text, translated_text_label)
-        self.assertDictEqual(response_json, fake_response)
-
-    def _mock_response_dictionary(self, academic_year, education_group_year, language, text_label, translated_text,
-                                  translated_text_label):
+    def _mock_response_dictionary(self, **kwargs):
         return {
-            'acronym': education_group_year.acronym,
-            'title': education_group_year.title_english,
-            'year': academic_year.year,
-            'language': language,
+            'acronym': kwargs['education_group_year'].acronym,
+            'title': kwargs['education_group_year'].title_english,
+            'year': kwargs['academic_year'].year,
+            'language': kwargs['language'],
             'sections': [
                 {
-                    'content': translated_text.text,
-                    'label': translated_text_label.label,
-                    'id': text_label.label
+                    'content': kwargs['translated_text'].text,
+                    'label': kwargs['translated_text_label'].label,
+                    'id': kwargs['text_label'].label
                 }
             ]
         }
