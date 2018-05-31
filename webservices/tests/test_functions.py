bd200171804f00286ed97fc2844ddf0df85cc399
Stéphane Wirtel <stephane@wirtel.be>
2018-05-31 22:01:47 +0200

    Remove the tests of a removed function

--- webservices/tests/test_functions.py
+++ webservices/tests/test_functions.py
@@ -30,7 +30,6 @@ from django.test import TestCase
 
 from base.tests.factories.education_group_year import EducationGroupYearFactory
 from webservices.views import get_title_of_education_group_year
-from webservices.utils import to_int_or_404
 
 
 class GetTitleOrEducationGroupYear_TestCase(TestCase):
@@ -42,13 +41,3 @@ class GetTitleOrEducationGroupYear_TestCase(TestCase):
 
         title = get_title_of_education_group_year(ega, 'en')
         self.assertEqual('english', title)
-
-
-class UtilsTestCase(unittest.TestCase):
-    def test_with_404(self):
-        with self.assertRaises(Http404):
-            to_int_or_404('salut')
-
-    def test_no_404(self):
-        result = to_int_or_404('10')
-        self.assertEqual(result, 10)
