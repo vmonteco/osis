633d59aa9783aba1fce2ed89696470cb2825b9ba
Stéphane Wirtel <stephane@wirtel.be>
2018-06-01 10:44:16 +0200

    Suppress the dependency rest_framework_swagger

--- webservices/urls.py
+++ webservices/urls.py
@@ -24,15 +24,11 @@
 #
 ##############################################################################
 from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
 
 from webservices.views import ws_catalog_offer
-
-schema_view = get_swagger_view(title='Training Catalog API')
 
 urlpatterns = [
     url('^v0.1/catalog/offer/(?P<year>[0-9]{4})/(?P<language>[a-zA-Z]{2})/(?P<acronym>[a-zA-Z0-9]+)$',
         ws_catalog_offer,
         name='v0.1-ws_catalog_offer'),
-    url(r'^doc$', schema_view),
 ]
