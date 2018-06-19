31254617c84672da2dc8924b559300bd66f595b8
Ndizera Eddy <eddyndizera@gmail.com>
2018-06-19 09:42:33 +0200

    OSIS-1323 Show success message upon creating education group year
    	modified:   base/locale/en/LC_MESSAGES/django.po
    	modified:   base/locale/fr_BE/LC_MESSAGES/django.po
    	modified:   base/views/education_groups/create.py

--- base/views/education_groups/create.py
+++ base/views/education_groups/create.py
@@ -25,22 +25,35 @@
 ##############################################################################
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from base.forms.education_group.create import CreateEducationGroupYearForm, CreateOfferYearEntityForm
 from base.models.person import Person
 from base.views import layout
+from base.views.common import display_success_messages
 
 
 @login_required
 @permission_required('base.can_create_education_group', raise_exception=True)
-def create_education_group(response):
-    person = get_object_or_404(Person, user=response.user)
-    form_education_group_year = CreateEducationGroupYearForm(response.POST or None)
-    form_offer_year_entity = CreateOfferYearEntityForm(response.POST or None)
+def create_education_group(request):
+    person = get_object_or_404(Person, user=request.user)
+    form_education_group_year = CreateEducationGroupYearForm(request.POST or None)
+    form_offer_year_entity = CreateOfferYearEntityForm(request.POST or None)
 
     if form_offer_year_entity.is_valid() and form_education_group_year.is_valid():
         education_group_year = form_education_group_year.save()
         form_offer_year_entity.save(education_group_year)
+
+        success_msg = create_success_message_for_creation_education_group_year(education_group_year)
+        display_success_messages(request, success_msg, extra_tags='safe')
         return redirect("education_group_read", education_group_year.id)
 
-    return layout.render(response, "education_group/creation.html", locals())
+    return layout.render(request, "education_group/creation.html", locals())
+
+
+def create_success_message_for_creation_education_group_year(education_group_year):
+    MSG_KEY = "Education group year <a href='%(link)s'> %(acronym)s (%(academic_year)s) </a> successfuly created."
+    link = reverse("education_group_read", args=[education_group_year.id])
+    return _(MSG_KEY) % {"link": link, "acronym": education_group_year.acronym,
+                         "academic_year": education_group_year.academic_year}
