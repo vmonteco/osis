8949537e3cde3735268e55bece717d581980ae0e
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-06-28 10:06:07 +0200

    #OSIS-1330 Créer le nécessaire pour update d'une offre de type TRAINING

--- base/forms/education_group/edition.py
+++ base/forms/education_group/edition.py
@@ -24,26 +24,18 @@
 #
 ##############################################################################
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
-from base.forms.learning_unit.entity_form import EntitiesVersionChoiceField
 from base.models import campus, education_group_type
 from base.models.education_group import EducationGroup
-from base.models.education_group_type import EducationGroupType
 from base.models.education_group_year import EducationGroupYear
-from base.models.entity_version import find_main_entities_version
-from base.models.enums import offer_year_entity_type, education_group_categories
-from base.models.group_element_year import GroupElementYear
-from base.models.offer_year_entity import OfferYearEntity
-from django.utils.translation import ugettext_lazy as _
 from base.models.enums import education_group_categories
-from base.forms.learning_unit.entity_form import EntitiesVersionChoiceField
-from base.models.offer_year_domain import OfferYearDomain
-from base.forms.utils.emptyfield import EmptyField
 from base.forms.education_group.create import CreateOfferYearEntityForm
 
 
 class EditionEducationGroupYearForm(forms.ModelForm):
     education_group_year = None
+
     class Meta:
         model = EducationGroupYear
         fields = ["acronym", "partial_acronym", "education_group_type", "title", "title_english",
@@ -60,43 +52,21 @@ class EditionEducationGroupYearForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.education_group_year = kwargs.get('instance', None)
-        self.instance = self.education_group_year
         super().__init__(*args, **kwargs)
         self.prepare_fields()
-        self.fields["academic_year"].required = False
-        self.fields["education_group"].required = False
 
     def prepare_fields(self):
+        self.fields["academic_year"].required = False
+        self.fields["education_group"].required = False
         self.fields["main_teaching_campus"].queryset = campus.find_main_campuses()
         self.fields["education_group_type"].queryset = \
-            education_group_type.find_by_category(education_group_categories.GROUP)
-
-    def save(self, **kwargs):
-        instance = super().save(**kwargs)
-        print(instance)
-        return instance
-    # def save(self, **kwargs):
-    #     print('save')
-    #     print(self.instance)
-    #     instance = super().save(**kwargs)
-    #     return instance
-    #
-    # def clean_academic_year(self):
-    #     print('clean_academic_year')
-    #     data_cleaned = self.cleaned_data.get('academic_year')
-    #     print(data_cleaned)
-    #     if data_cleaned:
-    #         return data_cleaned
+            education_group_type.find_by_category(education_group_categories.TRAINING)
+        self.fields["education_group_type"].required = True
 
     def clean(self):
-        print(self.instance)
-        print('clean')
         cleaned_data = self.cleaned_data
-
-        print(self.education_group_year.academic_year)
         cleaned_data['academic_year'] = self.education_group_year.academic_year
         cleaned_data['education_group'] = self.education_group_year.education_group
-        print(cleaned_data)
         return cleaned_data
 
 
@@ -105,25 +75,6 @@ class EducationGroupForm(forms.ModelForm):
     class Meta:
         model = EducationGroup
         fields = ['start_year', 'end_year']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-class EducationGroupForm(forms.ModelForm):
-
-    class Meta:
-        model = EducationGroup
-        fields = ['start_year', 'end_year']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-class EducationGroupTypeForm(forms.ModelForm):
-    class Meta:
-        model = EducationGroupType
-        fields = ['category', 'name']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- base/views/education_groups/update.py
+++ base/views/education_groups/update.py
@@ -23,34 +23,13 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
-from django.db import transaction
-from django.shortcuts import redirect, get_object_or_404
-from django.utils.translation import ugettext_lazy as _
-from waffle.decorators import waffle_flag
-
-from base.business.learning_unit_proposal import compute_proposal_state
-from base.forms.learning_unit.edition import LearningUnitEndDateForm
-from base.forms.learning_unit_proposal import ProposalLearningUnitForm, ProposalBaseForm
-from base.models.enums.proposal_type import ProposalType
-from base.models.learning_unit_year import LearningUnitYear
-from base.models.person import Person
-from base.models.proposal_learning_unit import ProposalLearningUnit
-from base.views import layout
-from base.views.common import display_success_messages
-from base.views.learning_units import perms
-from base.views.learning_units.common import get_learning_unit_identification_context
-from base.forms.education_group.edition import EditionEducationGroupYearForm, EducationGroupForm, \
-    EducationGroupTypeForm, UpdateOfferYearManagementEntityForm
-from base.models.education_group_year import EducationGroupYear
-from base.forms.education_group.create import CreateOfferYearEntityForm
-from django.forms import formset_factory
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from base.models.enums import education_group_categories
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import ugettext_lazy as _
 
+from base.forms.education_group.edition import EditionEducationGroupYearForm, EducationGroupForm, \
+    UpdateOfferYearManagementEntityForm
+from base.models.enums import education_group_categories
 from base.forms.education_group.create import CreateEducationGroupYearForm, CreateOfferYearEntityForm
 from base.models.education_group_year import EducationGroupYear
 from base.models.enums import offer_year_entity_type
@@ -65,15 +44,21 @@ from base.views.education_groups.perms import can_change_education_group
 def update_education_group(request, education_group_year_id):
     education_group_year = get_object_or_404(EducationGroupYear, pk=education_group_year_id)
     root = request.GET.get("root")
-
+    html_page = "education_group/update.html"
+    form_management_entity = None
+    form_education_group = None
     offer_year_entity = get_object_or_404(
         OfferYearEntity, education_group_year=education_group_year, type=offer_year_entity_type.ENTITY_ADMINISTRATION
     )
 
     if education_group_year.education_group_type.category == education_group_categories.GROUP:
         form_education_group_year = CreateEducationGroupYearForm(request.POST or None, instance=education_group_year)
-    elif education_group_year.education_group_type.category == education_group_categories.TRAINING:
+    else:
         form_education_group_year = EditionEducationGroupYearForm(request.POST or None, instance=education_group_year)
+        html_page = "education_group/identification_training_edit.html"
+        form_management_entity = UpdateOfferYearManagementEntityForm(
+            initial={'entity': education_group_year.management_entity})
+        form_education_group = EducationGroupForm(instance=education_group_year.education_group)
 
     form_offer_year_entity = CreateOfferYearEntityForm(request.POST or None, instance=offer_year_entity)
 
@@ -87,38 +72,10 @@ def update_education_group(request, education_group_year_id):
                                             query={"root": root})
         return redirect(url)
 
-    return layout.render(request, "education_group/update.html", {
+    return layout.render(request, html_page, {
         "form_education_group_year": form_education_group_year,
         "form_offer_year_entity": form_offer_year_entity,
         "education_group_year": education_group_year,
+        "form_management_entity": form_management_entity,
+        "form_education_group": form_education_group
     })
-
-
-@login_required
-def identification_edit(request, education_group_year_id):
-    education_group_year = get_object_or_404(EducationGroupYear, id=education_group_year_id)
-
-    if request.POST:
-        form = EditionEducationGroupYearForm(request.POST or None, instance=education_group_year)
-        if form.is_valid():
-            print('valid')
-            result = form.save()
-            print(result)
-            # display_success_messages(request, result, extra_tags='safe')
-            return HttpResponseRedirect(reverse('education_group_read', args=[result.id]))
-
-    else:
-        form = EditionEducationGroupYearForm(instance=education_group_year)
-
-    form_administrative_entity = CreateOfferYearEntityForm(initial={'entity': education_group_year.administration_entity})
-    form_management_entity = UpdateOfferYearManagementEntityForm(initial={'entity': education_group_year.management_entity})
-    form_education_group = EducationGroupForm(instance=education_group_year.education_group)
-    form_education_group_type = EducationGroupTypeForm(instance=education_group_year.education_group_type)
-
-    return layout.render(request, "education_group/identification_training_edit.html",
-                         {'education_group_year': education_group_year,
-                          'form': form,
-                          'form_administrative_entity': form_administrative_entity,
-                          'form_management_entity': form_management_entity,
-                          'form_education_group': form_education_group,
-                          'form_education_group_type': form_education_group_type})
