5527f143deee2472db2a84f5c5dae9bfc01f7a4d
Aurelien De Ryck <deryck.aurelien@gmail.com>
2018-07-02 18:56:38 +0200

    OSIS-1396 DeleteView for education_group_year

---
+++ base/views/education_groups/delete.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+
+from base.models.education_group_year import EducationGroupYear
+
+
+class DeleteGroupEducationYearView(PermissionRequiredMixin, DeleteView):
+    # DeleteView
+    model = EducationGroupYear
+    success_url = reverse_lazy('education_groups')
+    pk_url_kwarg = "education_group_year_id"
+    template_name = "education_group/delete.html"
+    context_object_name = "education_group_year"
+
+    # PermissionRequiredMixin
+    permission_required = "base.delete_educationgroupyear"
+    raise_exception = True
