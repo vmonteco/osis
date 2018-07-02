8237e204e6775b8693cfc919198c034186e68b92
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2018-07-02 14:26:21 +0200

    add education_group_year_parent filter in Form

--- base/forms/education_group/create.py
+++ base/forms/education_group/create.py
@@ -51,7 +51,8 @@ class CreateEducationGroupYearForm(forms.ModelForm):
 
         self.fields["main_teaching_campus"].queryset = campus.find_main_campuses()
         self.fields["education_group_type"].queryset = \
-            education_group_type.find_authorized_types(education_group_categories.GROUP)
+            education_group_type.find_authorized_types(category=education_group_categories.GROUP,
+                                                       education_group_year_parent=self.parent_education_group_year)
         self.fields["education_group_type"].required = True
 
         if self.parent_education_group_year:
