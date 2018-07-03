fbc89d67ca9674987db42c840135ed8b63dc3e4a
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2018-07-03 11:17:36 +0200

    OSIS-1411 Fix migration files for teachingmeterals

--- base/migrations/0300_auto_20180703_0941.py
+++ base/migrations/0300_auto_20180703_0941.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0298_unauthorizedrelationship'),
+        ('base', '0299_remove_learningunityear_bibliography'),
     ]
 
     operations = [
