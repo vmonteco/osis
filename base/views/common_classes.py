ed30f337d20053e598e6b6a1d7412ad5c2def286
Ndizera Eddy <eddyndizera@gmail.com>
2018-07-13 12:51:37 +0200

    OSIS-1509 Fixed pep8 errors
    	modified:   base/views/common_classes.py

--- base/views/common_classes.py
+++ base/views/common_classes.py
@@ -65,15 +65,15 @@ class RulesRequiredMixin(UserPassesTestMixin):
 
 class AjaxTemplateMixin(object):
 
-      def dispatch(self, request, *args, **kwargs):
-          if not hasattr(self, 'ajax_template_name'):
-              split = self.template_name.split('.html')
-              split[-1] = '_inner'
-              split.append('.html')
-              self.ajax_template_name = ''.join(split)
-          if request.is_ajax():
-              self.template_name = self.ajax_template_name
-          return super().dispatch(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(self, 'ajax_template_name'):
+            split = self.template_name.split('.html')
+            split[-1] = '_inner'
+            split.append('.html')
+            self.ajax_template_name = ''.join(split)
+        if request.is_ajax():
+            self.template_name = self.ajax_template_name
+        return super().dispatch(request, *args, **kwargs)
 
 
 class DeleteViewWithDependencies(FlagMixin, RulesRequiredMixin, AjaxTemplateMixin, DeleteView):
