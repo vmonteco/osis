9c03489640217acc45d47af664715efe77e53a49
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-07-24 14:19:14 +0200

    OSIS-1489 Reuse get_or_create_group_element_year function

--- base/views/education_groups/attach.py
+++ base/views/education_groups/attach.py
@@ -29,8 +29,8 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from waffle.decorators import waffle_flag
 
+from base.models import group_element_year
 from base.models.education_group_year import EducationGroupYear
-from base.models.group_element_year import GroupElementYear
 from base.utils.cache import cache_filter, cache
 from base.views.education_groups.perms import can_change_education_group
 from base.views.learning_units.perms import PermissionDecoratorWithUser
@@ -43,9 +43,9 @@ from base.views.learning_units.perms import PermissionDecoratorWithUser
 def education_group_attach(request, education_group_year_id):
     parent_id = int(request.GET.get('education_group_year_id'))
     child_id = int(cache.get('education_group_year_id'))
-    GroupElementYear.objects.get_or_create(
+    group_element_year.get_or_create_group_element_year(
         parent=EducationGroupYear.objects.get(id=parent_id),
-        child_branch=EducationGroupYear.objects.get(id=child_id)
+        child=EducationGroupYear.objects.get(id=child_id)
     )
     cache.set('education_group_year_id', None, timeout=None)
     return redirect(reverse('education_group_read', kwargs={'education_group_year_id': education_group_year_id}))
