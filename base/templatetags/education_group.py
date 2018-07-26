4092a46075aec60d827290e81c13bc49f1d6c76e
Aurelien De Ryck <deryck.aurelien@gmail.com>
2018-07-26 12:17:16 +0200

    OSIS-1577 Implement unittests

--- base/templatetags/education_group.py
+++ base/templatetags/education_group.py
@@ -25,7 +25,6 @@
 ##############################################################################
 from django import template
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
@@ -33,7 +32,9 @@ from base.business.education_groups.perms import is_eligible_to_delete_education
     is_eligible_to_change_education_group, is_eligible_to_add_education_group
 from base.models.education_group_year import EducationGroupYear
 
-register = template.Library()
+NO_GIVEN_ROOT = "INVALID TREE : no given root"
+ICON_JSTREE_FILE = "data-jstree='{\"icon\":\"jstree-icon jstree-file\"}'"
+
 
 # TODO use inclusion tag
 LI_TEMPLATE = """
@@ -64,7 +65,7 @@ ICONS = {
 
 BRANCH_TEMPLATE = """
 <ul>
-    <li {data_jstree} id="node_{id}">
+    <li {data_jstree} id="node_{gey}_{egy}">
         <a href="{url}" class="{a_class}">
             {text}
         </a>
@@ -72,6 +73,8 @@ BRANCH_TEMPLATE = """
     </li>
 </ul>
 """
+
+register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
@@ -145,32 +148,48 @@ def button_with_permission(context, title, id_a, value):
 
 @register.simple_tag(takes_context=True)
 def build_tree(context, current_group_element_year, selected_education_group_year):
-    root = context["request"].GET.get("root", "")
+    request = context["request"]
+    root = request.GET.get("root")
 
     # If it is the root, the group_element_year is not yet available.
     if not current_group_element_year:
-        education_group_year = get_object_or_404(EducationGroupYear, pk=root)
-        id_li = "root_" + str(education_group_year.pk)
+        try:
+            education_group_year = EducationGroupYear.objects.get(pk=root)
+        except (EducationGroupYear.MultipleObjectsReturned, EducationGroupYear.DoesNotExist):
+            return NO_GIVEN_ROOT
     else:
         education_group_year = current_group_element_year.child_branch
-        id_li = current_group_element_year.pk
 
-    if not education_group_year.group_element_year_branches:
-        data_jstree = "data-jstree='{\"icon\":\"jstree-icon jstree-file\"}'"
-    else:
-        data_jstree = ""
+    if not selected_education_group_year:
+        selected_education_group_year = education_group_year
 
-    a_class = "jstree-wholerow-clicked" if education_group_year.pk == selected_education_group_year.pk else ""
+    data_jstree = _get_icon_jstree(education_group_year)
+    a_class = _get_a_class(education_group_year, selected_education_group_year)
 
     chidren_template = ""
     for child in education_group_year.group_element_year_branches:
         chidren_template += build_tree(context, child, selected_education_group_year)
 
+    url_name = request.resolver_match.url_name if request.resolver_match else "education_group_read"
+
     return mark_safe(BRANCH_TEMPLATE.format(
         data_jstree=data_jstree,
-        id=id_li,
-        url=reverse(context["request"].resolver_match.url_name, args=[education_group_year.pk]) + "?root=" + root,
+        gey=current_group_element_year.pk if current_group_element_year else 0,
+        egy=education_group_year.pk,
+        url=reverse(url_name, args=[education_group_year.pk]) + "?root=" + (root or ""),
         text=education_group_year.verbose,
         a_class=a_class,
         children=chidren_template
     ))
+
+
+def _get_icon_jstree(education_group_year):
+    if not education_group_year.group_element_year_branches:
+        data_jstree = ICON_JSTREE_FILE
+    else:
+        data_jstree = ""
+    return data_jstree
+
+
+def _get_a_class(education_group_year, selected_education_group_year):
+    return "jstree-wholerow-clicked" if education_group_year.pk == selected_education_group_year.pk else ""
