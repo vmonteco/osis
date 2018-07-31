824834858d773ec14993dbfeea0e3ffc76ef4739
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-07-31 12:33:21 +0200

    #OSIS-1591 Suppression complète d'une offre

--- base/views/education_groups/delete.py
+++ base/views/education_groups/delete.py
@@ -27,24 +27,25 @@ from django.urls import reverse_lazy
 from django.utils.translation import ngettext_lazy
 from django.utils.translation import ugettext_lazy as _
 
+from base.models.education_group import EducationGroup
 from base.models.education_group_year import EducationGroupYear
 from base.models.group_element_year import GroupElementYear
 from base.models.offer_enrollment import OfferEnrollment
 from base.views.common_classes import DeleteViewWithDependencies
-from base.views.education_groups.perms import can_delete_education_group
+from base.views.education_groups.perms import can_delete_all_education_group
 
 
-class DeleteGroupEducationYearView(DeleteViewWithDependencies):
+class DeleteGroupEducationView(DeleteViewWithDependencies):
     # DeleteView
-    model = EducationGroupYear
+    model = EducationGroup
     success_url = reverse_lazy('education_groups')
     pk_url_kwarg = "education_group_year_id"
     template_name = "education_group/delete.html"
-    context_object_name = "education_group_year"
+    context_object_name = "education_group"
 
     # RulesRequiredMixin
     raise_exception = True
-    rules = [can_delete_education_group]
+    rules = [can_delete_all_education_group]
 
     # DeleteViewWithDependencies
     success_message = "The education group has been deleted."
@@ -52,13 +53,14 @@ class DeleteGroupEducationYearView(DeleteViewWithDependencies):
 
     # FlagMixin
     flag = 'education_group_delete'
+    education_group_years = []
 
-    # TODO : This method is a quick fix.
-    # GroupElementYear should be split in two tables with their own protected FK !
     def post_collect(self):
         for instance, obj in self.collector.model_objs.items():
             if instance is GroupElementYear:
                 self._append_protected_object(obj)
+
+        self._append_education_group_years()
 
     def _append_protected_object(self, list_objects):
         if not isinstance(list_objects, (list, set)):
@@ -72,27 +74,54 @@ class DeleteGroupEducationYearView(DeleteViewWithDependencies):
         context = super().get_context_data(**kwargs)
 
         if self.collector.protected:
+            context["education_group_years"] = self.education_group_years
             context["protected_messages"] = self.get_protected_messages()
-
         return context
 
     def get_protected_messages(self):
-        protected_message = []
+        if self.education_group_years:
+            return self._get_protected_messages_by_education_group_year(
+                sorted(list(self.education_group_years), key=lambda t: t.academic_year.year))
+        return []
 
-        count_enrollment = len([
-            enrollment for enrollment in self.collector.protected if isinstance(enrollment, OfferEnrollment)
-        ])
+    def _get_protected_messages_by_education_group_year(self, educ_sorted):
+        protected_messages = []
+        for educ in educ_sorted:
+            protected_message = []
+            count_enrollment = len([
+                                       enrollment for enrollment in self.collector.protected if
+                                       isinstance(enrollment,
+                                                  OfferEnrollment) and enrollment.education_group_year == educ
+                                       ])
 
-        if count_enrollment:
-            protected_message.append(
-                ngettext_lazy(
-                    "%(count_enrollment)d student is  enrolled in the offer.",
-                    "%(count_enrollment)d students are  enrolled in the offer.",
-                    count_enrollment
-                ) % {"count_enrollment": count_enrollment}
-            )
+            if count_enrollment:
+                protected_message.append(
+                    ngettext_lazy(
+                        "%(count_enrollment)d student is  enrolled in the offer.",
+                        "%(count_enrollment)d students are  enrolled in the offer.",
+                        count_enrollment
+                    ) % {"count_enrollment": count_enrollment}
+                )
 
-        if [isinstance(group, GroupElementYear) for group in self.collector.protected]:
-            protected_message.append(_("The content of the education group is not empty."))
+            for group in self.collector.protected:
+                if isinstance(group, GroupElementYear) and group.parent == educ:
+                    protected_message.append(_("The content of the education group is not empty."))
+                    break
+            usage_nb = 0
+            for group in self.collector.protected:
+                if isinstance(group, GroupElementYear) and group.child_branch == educ:
+                    usage_nb += 1
+                    if usage_nb >= 2:
+                        protected_message.append("{} : {}.".format(_('Used by an other education group'), educ))
+                        break
 
-        return protected_message
+            if protected_message:
+                protected_messages.append({'year': educ, 'messages': protected_message})
+        return protected_messages
+
+    def _append_education_group_years(self):
+        for obj in self.collector.nested():
+            if isinstance(obj, (list, set)):
+                for oo in obj:
+                    if type(oo) is EducationGroupYear:
+                        self.education_group_years.append(oo)
