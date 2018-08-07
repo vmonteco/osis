02737ba992ca940936ad7d344459709f8e5e8e41
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-08-07 15:04:38 +0200

    OSIS-1631 Add message also when request is not ajax

--- base/views/education_groups/select.py
+++ base/views/education_groups/select.py
@@ -24,6 +24,7 @@
 #
 ##############################################################################
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import redirect, get_object_or_404
@@ -43,9 +44,11 @@ from base.models.learning_unit_year import LearningUnitYear
 def education_group_select(request, root_id=None, education_group_year_id=None):
     education_group_year = get_object_or_404(EducationGroupYear, pk=request.POST['child_to_cache_id'])
     group_element_years.management.select_education_group_year(education_group_year)
+    success_message = _build_success_message(education_group_year)
     if request.is_ajax():
-        return _build_success_json_response(education_group_year)
+        return _build_success_json_response(success_message)
     else:
+        messages.add_message(request, messages.INFO, success_message)
         return redirect(reverse(
             'education_group_read',
             args=[
@@ -61,19 +64,24 @@ def education_group_select(request, root_id=None, education_group_year_id=None):
 def learning_unit_select(request, learning_unit_year_id):
     learning_unit_year = get_object_or_404(LearningUnitYear, pk=learning_unit_year_id)
     group_element_years.management.select_learning_unit_year(learning_unit_year)
+    success_message = _build_success_message(learning_unit_year)
     if request.is_ajax():
-        return _build_success_json_response(learning_unit_year)
+        return _build_success_json_response(success_message)
     else:
+        messages.add_message(request, messages.INFO, success_message)
         return redirect(reverse(
             'learning_unit',
             args=[learning_unit_year_id]
         ))
 
 
-def _build_success_json_response(obj):
-    success_message = """{} : "{}" """.format(
+def _build_success_message(obj):
+    return """{} : "{}" """.format(
         _("Selected element"),
         str(obj)
     )
+
+
+def _build_success_json_response(success_message):
     data = {'success_message': success_message}
     return JsonResponse(data)
