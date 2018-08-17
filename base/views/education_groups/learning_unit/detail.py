1237ea3406fe30601779d80c2af224c9b63776c6
Ndizera Eddy <eddyndizera@gmail.com>
2018-08-17 14:35:39 +0200

    OSIS-1687 Display tree for learning unit
    	modified:   base/templates/education_group/learning_unit/tab_utilization.html
    	modified:   base/views/education_groups/learning_unit/detail.py

--- base/views/education_groups/learning_unit/detail.py
+++ base/views/education_groups/learning_unit/detail.py
@@ -1,17 +1,44 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 
+from base.models.education_group_year import EducationGroupYear
 from base.models.learning_unit_year import LearningUnitYear
+from base.models.person import Person
 
 
-class LearningUnitUtilization(DetailView):
+@method_decorator(login_required, name='dispatch')
+class LearningUnitGenericDetailView(PermissionRequiredMixin, DetailView):
+    # DetailView
     model = LearningUnitYear
-    template_name = "education_group/learning_unit/tab_utilization.html"
-
+    context_object_name = "learning_unit_year"
     pk_url_kwarg = 'learning_unit_year_id'
 
+    # PermissionRequiredMixin
+    permission_required = 'base.can_access_education_group'
+    raise_exception = True
+
+    def get_person(self):
+        return get_object_or_404(Person, user=self.request.user)
+
+    def get_root(self):
+        return get_object_or_404(EducationGroupYear, pk=self.kwargs.get("root_id"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["learning_unit_year"] = self.get_object()
+        # This objects are mandatory for all education group views
+        context['person'] = self.get_person()
+
+        context['root'] = self.get_root()
+        context['root_id'] = self.kwargs.get("root_id")
+        context['parent'] = self.get_root()
+
+        context['group_to_parent'] = self.request.GET.get("group_to_parent") or '0'
         return context
+
+
+class LearningUnitUtilization(LearningUnitGenericDetailView):
+    template_name = "education_group/learning_unit/tab_utilization.html"
