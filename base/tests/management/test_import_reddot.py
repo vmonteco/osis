5b6e9c5f9e1b52391817df235ecf14782822a718
Stéphane Wirtel <stephane@wirtel.be>
2018-08-17 09:40:19 +0200

    Add a test for base.managements.commands.import_reddot.Command#load_admission_conditions_for_bachelor

---
+++ base/tests/management/test_import_reddot.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+from base.management.commands import import_reddot
+from base.models.admission_condition import AdmissionCondition
+from base.models.education_group_year import EducationGroupYear
+from base.tests.factories.education_group_year import EducationGroupYearFactory
+
+
+class ImportReddotTestCase(TestCase):
+
+    def test_load_admission_conditions_for_bachelor(self):
+        education_group_year_common = EducationGroupYearFactory(acronym='common')
+        item = {
+            'year': education_group_year_common.academic_year.year,
+            'acronym': 'bacs',
+            'info': {
+                'alert_message': {'text-common': 'Alert Message'},
+                'ca_bacs_cond_generales': {'text-common': 'General Conditions'},
+                'ca_bacs_cond_particulieres': {'text-common': 'Specific Conditions'},
+                'ca_bacs_examen_langue': {'text-common': 'Language Exam'},
+                'ca_bacs_cond_speciales': {'text-common': 'Special Conditions'},
+            }
+        }
+
+        command = import_reddot.Command()
+
+        lang = 'fr-be'
+        command.suffix_language = '' if lang == 'fr-be' else '_en'
+
+        command.load_admission_conditions_for_bachelor(item, education_group_year_common.academic_year.year)
+
+        common_bacs = EducationGroupYear.objects.filter(academic_year=education_group_year_common.academic_year,
+                                                        acronym='common-bacs').first()
+
+        admission_condition = AdmissionCondition.objects.get(education_group_year=common_bacs)
+        self.assertEqual(admission_condition.text_alert_message,
+                         item['info']['alert_message']['text-common'])
+        self.assertEqual(admission_condition.text_ca_bacs_cond_generales,
+                         item['info']['ca_bacs_cond_generales']['text-common'])
+        self.assertEqual(admission_condition.text_ca_bacs_cond_particulieres,
+                         item['info']['ca_bacs_cond_particulieres']['text-common'])
+        self.assertEqual(admission_condition.text_ca_bacs_examen_langue,
+                         item['info']['ca_bacs_examen_langue']['text-common'])
+        self.assertEqual(admission_condition.text_ca_bacs_cond_speciales,
+                         item['info']['ca_bacs_cond_speciales']['text-common'])
