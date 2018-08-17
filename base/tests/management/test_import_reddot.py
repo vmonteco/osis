90a2716d508094d5ffdeda56d20b44250c12a7e5
Stéphane Wirtel <stephane@wirtel.be>
2018-08-17 10:41:19 +0200

    Add a test for base.managements.commands.import_reddot.Command#save_condition_line_of_row

--- base/tests/management/test_import_reddot.py
+++ base/tests/management/test_import_reddot.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
 
 from base.management.commands import import_reddot
-from base.models.admission_condition import AdmissionCondition
+from base.models.admission_condition import AdmissionCondition, AdmissionConditionLine
 from base.models.education_group_year import EducationGroupYear
 from base.tests.factories.education_group_year import EducationGroupYearFactory
 
 
 class ImportReddotTestCase(TestCase):
+    def setUp(self):
+        self.command = import_reddot.Command()
+        lang = 'fr-be'
+        self.command.suffix_language = '' if lang == 'fr-be' else '_en'
 
     def test_load_admission_conditions_for_bachelor(self):
         education_group_year_common = EducationGroupYearFactory(acronym='common')
@@ -22,12 +26,7 @@ class ImportReddotTestCase(TestCase):
             }
         }
 
-        command = import_reddot.Command()
-
-        lang = 'fr-be'
-        command.suffix_language = '' if lang == 'fr-be' else '_en'
-
-        command.load_admission_conditions_for_bachelor(item, education_group_year_common.academic_year.year)
+        self.command.load_admission_conditions_for_bachelor(item, education_group_year_common.academic_year.year)
 
         common_bacs = EducationGroupYear.objects.filter(academic_year=education_group_year_common.academic_year,
                                                         acronym='common-bacs').first()
@@ -43,3 +42,31 @@ class ImportReddotTestCase(TestCase):
                          item['info']['ca_bacs_examen_langue']['text-common'])
         self.assertEqual(admission_condition.text_ca_bacs_cond_speciales,
                          item['info']['ca_bacs_cond_speciales']['text-common'])
+
+    def test_save_condition_line_of_row(self):
+        education_group_year = EducationGroupYearFactory()
+
+        admission_condition = AdmissionCondition.objects.create(education_group_year=education_group_year)
+
+        self.assertEqual(AdmissionConditionLine.objects.filter(admission_condition=admission_condition).count(), 0)
+
+        line = {
+            'type': 'table',
+            'title': 'ucl_bachelors',
+            'diploma': 'Diploma',
+            'conditions': 'Conditions',
+            'access': 'Access',
+            'remarks': 'Remarks',
+            'external_id': '1234567890'
+        }
+        self.command.save_condition_line_of_row(admission_condition, line)
+
+        queryset = AdmissionConditionLine.objects.filter(admission_condition=admission_condition)
+        self.assertEqual(queryset.count(), 1)
+
+        admission_condition_line = queryset.first()
+
+        self.assertEqual(admission_condition_line.diploma, line['diploma'])
+        self.assertEqual(admission_condition_line.conditions, line['conditions'])
+        self.assertEqual(admission_condition_line.access, line['access'])
+        self.assertEqual(admission_condition_line.remarks, line['remarks'])
