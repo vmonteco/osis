63a2f965dbd4624462a5ff2dcc8b7d1a5c3e5ec6
Aurélien De Ryck <aurelien.deryck@usaintlouis.be>
2018-08-23 14:29:21 +0200

    OSIS-1700 Create tests folder for coverage

--- rules_management/tests/test_permission_field_mixin.py
+++ rules_management/tests/test_permission_field_mixin.py
@@ -28,7 +28,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
 
-from rules_management.fatories import PermissionFactory, FieldReferenceFactory
+from rules_management.tests.fatories import PermissionFactory, FieldReferenceFactory
 from rules_management.mixins import PermissionFieldMixin
 from base.tests.factories.user import UserFactory
 from reference.models.country import Country
