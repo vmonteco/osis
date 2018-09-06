cfd618487c2684994b184cb0adb78d01a42f7c1b
Stéphane Wirtel <stephane@wirtel.be>
2018-09-06 16:17:59 +0200

    Remove prettyprinter

--- base/views/education_group.py
+++ base/views/education_group.py
@@ -35,7 +35,6 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.http import require_http_methods
-from prettyprinter import cpprint
 from waffle.decorators import waffle_flag
 
 from base import models as mdl
