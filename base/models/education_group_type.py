627f0755acfdaa6ac0a98b4c144ce7b5b4aea159
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2018-09-11 16:52:49 +0200

    OSIS-1838 rename type to category in select/update form

--- base/models/education_group_type.py
+++ base/models/education_group_type.py
@@ -71,7 +71,7 @@ class EducationGroupType(models.Model):
         max_length=25,
         choices=education_group_categories.CATEGORIES,
         default=education_group_categories.TRAINING,
-        verbose_name=_('type'),
+        verbose_name=_('category'),
     )
 
     name = models.CharField(
