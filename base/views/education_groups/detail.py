7b86ca150794473e2720e676a29f588f3d4f5db5
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2018-09-14 14:11:39 +0200

    OSIS-1890 Implement view for panel achievements

--- base/views/education_groups/detail.py
+++ base/views/education_groups/detail.py
@@ -350,3 +350,12 @@ class EducationGroupYearAdmissionCondition(EducationGroupGenericDetailView):
         })
 
         return context
+
+
+class EducationGroupSkillsAchievements(EducationGroupGenericDetailView):
+    template_name = "education_group/tab_skills_achievements.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'LANGUAGE_CODE_FR': settings.LANGUAGE_CODE_FR, 'LANGUAGE_CODE_EN': settings.LANGUAGE_CODE_EN})
+        return context
