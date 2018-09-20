2d325cff3927d62cd1d381cb86eb7ca1ff60d1ad
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-09-20 12:09:37 +0200

    OSIS-1917 Fixed test test_my_osis

--- base/tests/views/test_my_osis.py
+++ base/tests/views/test_my_osis.py
@@ -35,6 +35,7 @@ from attribution.tests.factories.attribution import AttributionFactory
 from base.models.enums import academic_calendar_type
 from base.tests.factories.academic_calendar import AcademicCalendarFactory
 from base.tests.factories.academic_year import create_current_academic_year
+from base.tests.factories.learning_container_year import LearningContainerYearFactory
 from base.tests.factories.learning_unit_year import LearningUnitYearFakerFactory
 from base.tests.factories.person import PersonFactory
 from base.tests.factories.tutor import TutorFactory
@@ -65,7 +66,9 @@ class MyOsisViewTestCase(TestCase):
 
         self.tutor = TutorFactory(person=self.person)
 
-        self.learning_unit_year = LearningUnitYearFakerFactory(academic_year=academic_year)
+        self.learning_container_year = LearningContainerYearFactory(academic_year=academic_year)
+        self.learning_unit_year = LearningUnitYearFakerFactory(academic_year=academic_year,
+                                                               learning_container_year=self.learning_container_year)
         self.attribution = AttributionFactory(learning_unit_year=self.learning_unit_year, summary_responsible=True,
                                               tutor=self.tutor)
 
