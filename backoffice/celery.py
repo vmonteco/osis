648858200bbe0235442567ddf360e5478d920295
Ndizera Eddy <eddyndizera@gmail.com>
2018-09-28 14:39:35 +0200

    celery Separate celery settings
    	modified:   backoffice/__init__.py
    	modified:   backoffice/celery.py

--- backoffice/celery.py
+++ backoffice/celery.py
@@ -23,7 +23,21 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+from __future__ import absolute_import, unicode_literals
+import os
+import sys
+
+import dotenv
 from celery import Celery
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if 'test' in sys.argv:
+    os.environ.setdefault('TESTING', 'True')
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
+sys.path.extend(os.environ.get('EXTRA_SYS_PATHS').split()) if os.environ.get('EXTRA_SYS_PATHS') else None
+
+SETTINGS_FILE = os.environ.get('DJANGO_SETTINGS_MODULE', 'backoffice.settings.local')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_FILE)
 
 app = Celery('celery_app')
 
