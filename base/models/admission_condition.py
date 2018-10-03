9df9f73335a7c5fe58ce03fc4c92bac02c5875a9
Stéphane Wirtel <stephane@wirtel.be>
2018-10-03 11:49:04 +0200

    Use %s instead .format for the error message

--- base/models/admission_condition.py
+++ base/models/admission_condition.py
@@ -108,7 +108,7 @@ class AdmissionConditionLine(OrderedModel):
         super().save(*args, **kwargs)
 
         if self.access not in dict(CONDITION_ADMISSION_ACCESSES):
-            raise ValidationError({'access': _('{} is not an accepted value').format(self.access)})
+            raise ValidationError({'access': _('%s is not an accepted value') % (self.access,)})
 
 
 class AdmissionConditionLineAdmin(osis_model_admin.OsisModelAdmin):
