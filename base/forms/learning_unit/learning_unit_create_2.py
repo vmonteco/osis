f5dd428029af7de9a034e97f037b875fa0618923
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-10-07 13:32:56 +0200

    OSIS-1790 Fixed PEP8 Code Climate Warning

--- base/forms/learning_unit/learning_unit_create_2.py
+++ base/forms/learning_unit/learning_unit_create_2.py
@@ -227,7 +227,8 @@ class FullForm(LearningUnitBaseForm):
     def _restrict_academic_years_choice(self, postposal):
         if postposal:
             starting_academic_year = academic_year.starting_academic_year()
-            end_year_range = MAX_ACADEMIC_YEAR_FACULTY if self.person.is_faculty_manager() else MAX_ACADEMIC_YEAR_CENTRAL
+            end_year_range = MAX_ACADEMIC_YEAR_FACULTY if self.person.is_faculty_manager() \
+                else MAX_ACADEMIC_YEAR_CENTRAL
 
             self.fields["academic_year"].queryset = academic_year.find_academic_years(
                 start_year=starting_academic_year.year,
