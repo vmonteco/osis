e494c4db2a623bd590fbfdccd73f2e7fc9cdf30d
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-10-18 08:06:09 +0200

    #OSIS-1991 Migration file correction

--- base/migrations/0373_education_group_organization_verbose.py
+++ base/migrations/0373_education_group_organization_verbose.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0366_auto_20181010_1627'),
+        ('base', '0372_auto_20181017_1601'),
     ]
 
     operations = [
